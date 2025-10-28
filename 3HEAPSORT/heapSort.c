c7df3d7f347b3cfc47cba4d5b4a5ae39278c0c34
KASA <kasasachith@gmail.com>
2025-10-28 15:47:50 +0530

    completed heap sort program successfully

--- 3HEAPSORT/heapSort.c
+++ 3HEAPSORT/heapSort.c
@@ -8,3 +8,58 @@ void swap (int *a, int *b) {
     *b = temp;
 }
 
+void heapify (int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort (int arr[], int n) {
+    int i;
+
+    for (i = n/2-1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (i = n-1; i >=0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray (int arr[], int n) {
+    printf("Sorted Heap Sort is: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main () {
+    int arr[100], i, n;
+
+    printf("Enter the size of the heap sort: ");
+    scanf("%d", &n);
+
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+
+
+    heapSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
